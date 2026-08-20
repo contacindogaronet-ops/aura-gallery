43073de30b40ad8a896f95a66e5c21eff4789dd2
contacindogaronet-ops <contacindogaro.net@gmail.com>
2026-08-20 23:12:10 +0700

    feat: setup jetpack compose engine and basic ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,14 @@ android {
         versionName = "1.0"
     }
     
+    buildFeatures {
+        compose = true
+    }
+    
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.9"
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -27,4 +35,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.6.1")
+    implementation("androidx.compose.material3:material3:1.2.0")
 }
